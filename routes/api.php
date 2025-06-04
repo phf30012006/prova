43fb8f3b5e91e9a975a289b943cbafff0f933148
PHFUSCO\paulo <paulohfusco@gmail.com>
2025-06-04 15:27:56 -0300

    patch

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::controller(LivroController::class)->group(function () {
     Route::get('/livros', 'get');
     Route::get('/livros/{id}', 'details');
     Route::post('/livros','store');
-    Route::put('/livros/{id}', 'update');
+    Route::patch('/livros/{id}', 'update');
     Route::delete('/livros/{id}', 'delete');
     Route::get('/livros/{id}/reviews','getReviews');
 });
@@ -28,7 +28,7 @@ Route::controller(AutorController::class)->group(function () {
     Route::get('/autores', 'get');
     Route::get('/autores/{id}', 'details');
     Route::post('/autores', 'store');
-    Route::put('/autores/{id}', 'update');
+    Route::patch('/autores/{id}', 'update');
     Route::delete('/autores/{id}', 'delete');
     Route::get('/autores/{id}/livros', 'getLivrosPorAutor');
 });
@@ -38,7 +38,7 @@ Route::controller(GeneroController::class)->group(function () {
     Route::get('/generos', 'get');
     Route::get('/generos/{id}', 'details');
     Route::post('/generos', 'store');
-    Route::put('/generos/{id}', 'update');
+    Route::patch('/generos/{id}', 'update');
     Route::delete('/generos/{id}', 'delete');
     Route::get('/generos/{id}/livros', 'getLivrosPorGenero');
 });
@@ -47,7 +47,7 @@ Route::controller(UsuarioController::class)->group(function () {
     Route::get('/usuarios', 'get');
     Route::get('/usuarios/{id}', 'details');
     Route::post('/usuarios', 'store');
-    Route::put('/usuarios/{id}', 'update');
+    Route::patch('/usuarios/{id}', 'update');
     Route::delete('/usuarios/{id}', 'delete');
     Route::get('/usuarios/{id}/reviews', 'getReviewsPorUsuario');
 });
@@ -56,6 +56,6 @@ Route::controller(ReviewController::class)->group(function () {
     Route::get('/reviews', 'get');
     Route::get('/reviews/{id}', 'details');
     Route::post('/reviews', 'store');
-    Route::put('/reviews/{id}', 'update');
+    Route::patch('/reviews/{id}', 'update');
     Route::delete('/reviews/{id}', 'delete');
 });
